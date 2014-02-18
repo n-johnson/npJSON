67c6d11d87f273712b397297112fe26eea12dab0
Nathan Johnson <bitbucket@njohnson.me>
2014-02-17 22:20:26 -0600

    First functional version. Switched to fs.writeFile() over fs.write() and reads. Testing is more comprehensive.

--- npJSON.js
+++ npJSON.js
@@ -9,11 +9,11 @@
 var fs = require("fs");
 
 /**
- * [npJSON]
- * @param  {[string]} fileName [file where data is stored]
+ * npJSON
+ * @param  {string} fileName - file where data is stored
  */
 var npJSON = function(fileName) {
-	if(typeof fileName === 'undefined' || fileName === '')
+	if (typeof fileName === 'undefined' || fileName === '')
 		throw "ERROR: filename given to npJSON was empty.";
 	this.fileName = fileName;
 	this.loaded = false;
@@ -23,118 +23,158 @@ var npJSON = function(fileName) {
 };
 
 /**
- * [_initialize - opens stream to fileName; creates file if doesn't exist ]
+ * _initialize - opens fileName; creates file if doesn't exist
  *  @private
  */
 npJSON.prototype._initialize = function() {
-	if(!fs.existsSync(this.fileName)) { //Create the database
+	if (!fs.existsSync(this.fileName)) {
 		var tempFile = fs.openSync(this.fileName, "w"), //Open and write {} to create a (blank) valid JSON object
 			tempBuffer = new Buffer('{}');
-		fs.writeSync(tempFile,tempBuffer,0,2,0);
+		fs.writeSync(tempFile, tempBuffer, 0, 2, 0);
 		fs.closeSync(tempFile);
 	}
-		this.storageFile = fs.readFileSync(this.fileName); //Syncronous because we DO want this to be a blocking event.
+	this._loadData();
 };
 
 /**
- * [_readFile - Reads data stream ]
+ * _readFile - Reads data
  * @private
- * @return {[string]} [Returns the file as a utf-8 string]
+ * @return {string} - Returns the file as a utf-8 string
  */
 npJSON.prototype._readFile = function() {
-	var fileStats = fs.statSync(this.fileName); //Load file stats to determine bytes to read
-	this.fileSize = fileStats.size;
-	var fileContents = new Buffer(this.fileSize); //Create a buffer to load file contents into
-	var filePosition = 0, //Start at 0, this leaves room to create indexes in the future
-		bytesToRead = 512; //How many bytes to read per iteration. What is the ideal number???
-
-	while (filePosition < this.fileSize) {
-		bytesToRead = ((filePosition + bytesToRead) > this.fileSize) ? (this.fileSize - filePosition) : bytesToRead; //Ensure we don't attempt to read more bytes than actually exist
-		fs.readSync(this.storageFile, fileContents, filePosition, bytesToRead, filePosition); //Pushes data to buffer
-		filePosition += bytesToRead;
-	}
-	var rData = fileContents.toString('utf8', 0, this.fileSize);
-
-	return rData;
+	return fs.readFileSync(this.fileName); //Syncronous because we DO want this to be a blocking event.
 };
 
 /**
- * [_writeFile - Writes updated data to file]
- * @param  {[object]} newData [The data to be written]
- * @param  {[function]} [optionalCallBack] [Used with writeDelayedData to reset]
- * @private
- * @note Data should be stored in object before it is written to a file to ensure that if any changes occur while updating they are still written to memory, and thus only the last change needs to be written to file, as it will contain data from all the previous changes
- */
-npJSON.prototype._writeFile = function(newData) {
-	if (!this.loaded)
-		throw "ERROR: npJSON._writeFile() was called before any data was loaded.";
-	var stringToWrite = JSON.stringify(newData),
-		dataBuffer = new Buffer(stringToWrite),
-		that = this;
-
-	if(this._writeLock) { //Already writing data, wait until complete
-		this._writeDelayedData = JSON.stringify(newData); //We are storing it as a JSON string because we need to delete the data before we try to write the file in case any new data comes in while the data is being written, and if we used an object, we would have to duplicate it, and this is more reliable
-	}
-
-	this._writeLock = true; //Prevent npJSON from trying to access file again until current write is complete
-	fs.write(this.storageFile, dataBuffer, 0, stringToWrite.length, 0, function(err, written, buffer) {
-		if (err)
-			throw err;
-		if(that._writeDelayedData !== null) { //Data to write has piled up, lets process it
-			var latestData = that._writeDelayedData;
-			that._writeDelayedData = null;
-			that._writeFile(JSON.parse(that._writeDelayedData)); //For simplicity sake just using JSON as opposed to duplicating objects. TODO: test to ensure this isn't as much of a performance hit as duplicating large objects would be
-		}
-		that._writeLock = false;
-	});
-};
-
-/**
- * [_loadData - Creates an object representation of the string returned in _readFile()]
+ * _loadData - Creates an object representation of the string returned in _readFile()
  * @private
  */
 npJSON.prototype._loadData = function() {
 	var rawContents = this._readFile();
 	try {
 		this._rawObject = JSON.parse(rawContents);
-	} catch(e) {
+	} catch (e) {
 		throw "ERROR: npJSON: specified file does not contain valid JSON data";
 	}
 	this.loaded = true;
 	this._writeLock = false;
 };
 
+/**
+ * _writeFile - Writes updated data to file
+ * @param  {object} newData - The data to be written
+ * @param  {function} optionalCallBack - Used with writeDelayedData to reset
+ * @private
+ */
+npJSON.prototype._writeFile = function(newData, callBack) {
+	callBack = callBack || function() {};
+	if (!this.loaded)
+		throw "ERROR: npJSON._writeFile() was called before any data was loaded.";
+	var stringToWrite = JSON.stringify(newData),
+		that = this;
+
+	if (this._writeLock) {
+		this._writeDelayedData = JSON.stringify(newData); //We are storing it as a JSON string because we need to delete the data before we try to write the file in case any new data comes in while the data is being written, and if we used an object, we would have to duplicate it, and this is more reliable
+	}
+
+	this._writeLock = true; //Prevent npJSON from trying to access file again until current write is complete
+	fs.writeFile(this.fileName, stringToWrite, function(err) {
+		if (err)
+			throw err;
+		if (that._writeDelayedData !== null) {
+			var latestData = that._writeDelayedData;
+			that._writeDelayedData = null;
+			that._writeFile(JSON.parse(latestData)); //For simplicity sake just using JSON as opposed to duplicating objects. TODO: test to ensure this isn't as much of a performance hit as duplicating large objects would be
+		}
+		that._writeLock = false;
+		callBack(); //Issue callback
+	});
+};
+
+/**
+ * forceReload - Reloads data from file, destroying what was in memory.
+ *               Possible to lose data if it hadn't been written, use with caution
+ */
+npJSON.prototype.forceReload = function() {
+	this._writeLock = true; //Don't let anything new be added to prevent data loss
+	this._rawObject = {};
+	this._loadData();
+};
+
+/**
+ * getValue - Returns value of a given key
+ * @param  {string} key
+ * @return {any} - Value of given key
+ */
 npJSON.prototype.getValue = function(key) {
-	if(!this.loaded)
+	if (!this.loaded)
 		throw "ERROR: npJSON.getValue() was called before any data was loaded.";
-	if(typeof this._rawObject[key] !== 'undefined') //Check existance of object before returning it
+	if (typeof this._rawObject[key] !== 'undefined')
 		return this._rawObject[key];
 	return null;
 };
 
-npJSON.prototype.updateValue = function(key, value) {
-	if(!this.loaded)
+/**
+ * updateValue - Updates an existing value by a given key
+ *               If value doesn't exist, will create it.
+ * @param  {string} key
+ * @param  {any} value
+ * @param {function} callBack - OPTIONAL: Called after data is written to file. No parameters set.
+ */
+npJSON.prototype.updateValue = function(key, value, callBack) {
+	callBack = callBack || function() {};
+	if (!this.loaded)
 		throw "ERROR: npJSON.updateValue() was called before any data was loaded.";
-	this.setValue(key,value,true);
+	this.setValue(key, value, callBack, true);
 };
 
-npJSON.prototype.setValue = function(key, value, overWrite) {
-	overWrite = overWrite || false;
-	if(!overWrite) {
-		if(this.getValue(key) !== null)
+/**
+ * setValue - Sets a value for a key
+ *            Will throw exception if key already exists. Use updateValue for existing keys.
+ * @param {string} key
+ * @param {any} value
+ * @param {function} callBack - OPTIONAL: Called after data is written to file. No parameters set.
+ * @param {boolean} _overWrite - OPTIONAL: Only should be used by updateValue. Leave blank.
+ */
+npJSON.prototype.setValue = function(key, value, callBack, _overWrite) {
+	_overWrite = _overWrite || false;
+	callBack = callBack || function() {};
+	if (!_overWrite) {
+		if (this.getValue(key) !== null)
 			throw "ERROR: npJSON: Attempted to overwrite data, make sure updateValue() is used instead of setValue for already written data";
 	}
-	if(!this.loaded)
+	if (!this.loaded)
 		throw "ERROR: npJSON.setValue() was called before any data was loaded.";
-	this._rawObject[key] = value; //Add to memory
-	this._writeFile(this._rawObject); //Write it to file
+	this._rawObject[key] = value;
+	this._writeFile(this._rawObject, callBack);
 };
 
-npJSON.prototype.removeValue = function(key) {
-	if(!this.loaded)
+/**
+ * setValues - Sets many values at once.
+ * @param {array} arrayOfArrays - Data should be in the form [[key,value],[key,value],[key,value],etc]
+ * @param {function} callBack - OPTIONAL: Called after data is written to file. No parameters set.
+ */
+npJSON.prototype.setValues = function(arrayOfArrays, callBack) {
+	callBack = callBack || function() {};
+	if (!this.loaded)
+		throw "ERROR: npJSON.setValue() was called before any data was loaded.";
+	for (var i = 0; i < arrayOfArrays.length; i++) {
+		this._rawObject[arrayOfArrays[i][0]] = arrayOfArrays[i][1]; //Add to memory
+	}
+	this._writeFile(this._rawObject, callBack); //Write it to file
+};
+
+/**
+ * removeValue - Removes a KV pair from data set
+ * @param  {string} key - key to remove
+ * @param {function} callBack - OPTIONAL: Called after data is written to file. No parameters set.
+ */
+npJSON.prototype.removeValue = function(key, callBack) {
+	callBack = callBack || function() {};
+	if (!this.loaded)
 		throw "ERROR: npJSON.removeValue() was called before any data was loaded.";
 	delete this._rawObject[key];
-	this._writeFile(this._rawObject); //Write it to file
+	this._writeFile(this._rawObject, callBack);
 };
 
 module.exports = function(fileName) {
--- test.js
+++ test.js
@@ -1,12 +1,146 @@
-var file = require("./npJSON")('test');
-var data = file._loadData();
+/*!
+ * npJSON v0.0.1
+ * Author: Nathan Johnson <node@njohnson.me>
+ * License: MIT Licensed
+ *
+ * File: test.js
+ */
 
-//console.log(file._rawObject);
+//Create test file with random name - initialize
+var filename = 'test.' + getRandomText(5, 6);
+var file = require("./npJSON")(filename);
+var fs = require("fs");
 
-console.log(file.getValue('a'));
+console.log('Begin testing. Using file: ' + filename);
+test1(); //Begin chain of tests.
 
-//file.setValue('b','123');
-file.updateValue('b','456');
+//Write data to it
+//Ensure we are using all data types
+function test1() {
+	file.setValue("abc", 123, function() {
+		file.setValue("123", 'abc', function() {
+			file.setValue("t", true, function() {
+				file.setValue("f", false, function() {
+					file.setValue("obj", {
+						"inner_abc": 123,
+						"inner_123": "abc",
+						"inner_t": true,
+						"inner_f": false
+					}, function() {
+						check1();
+					});
+				});
+			});
+		});
+	});
+}
 
+//Let's check the data
+function check1() {
+	file.forceReload();
 
-file.close();
+	var tmp1 = file.getValue("abc");
+	if (tmp1 !== 123)
+		throw "Error: check1.tmp1";
+	var tmp2 = file.getValue("123");
+	if (tmp2 !== 'abc')
+		throw "Error: check1.tmp2";
+	var tmp3 = file.getValue("t");
+	if (tmp3 !== true)
+		throw "Error: check1.tmp3";
+	var tmp4 = file.getValue("f");
+	if (tmp4 !== false)
+		throw "Error: check1.tmp4";
+	var tmp5 = file.getValue("obj");
+	var tmp5obj = {
+		"inner_abc": 123,
+		"inner_123": "abc",
+		"inner_t": true,
+		"inner_f": false
+	};
+	if (JSON.stringify(tmp5) !== JSON.stringify(tmp5obj))
+		throw "Error: check1.tmp5";
+
+	console.log("Check 1 passed. Data types written and read successfully.");
+	test2();
+}
+
+function test2() {
+	var testArray = [
+		['s1', 'abc'],
+		['s2', 123],
+		['s3', false]
+	];
+	file.setValues(testArray, check2);
+}
+
+function check2() {
+	file.forceReload();
+	var tmp1 = file.getValue("s1");
+	if (tmp1 !== 'abc')
+		throw "Error: check2.tmp1";
+	var tmp2 = file.getValue("s2");
+	if (tmp2 !== 123)
+		throw "Error: check2.tmp2";
+	var tmp3 = file.getValue("s3");
+	if (tmp3 !== false)
+		throw "Error: check2.tmp3";
+	console.log("Check 2 passed. setValues() function works as expected.");
+	test3();
+}
+
+//Remove, update, change, etc data, and verify it exists as expected
+function test3() {
+	file.updateValue('abc', 456, function() {
+		file.updateValue('t', false, function() {
+			file.removeValue("123", function() {
+				check3();
+			});
+		});
+	});
+}
+
+function check3() {
+	file.forceReload();
+
+	var tmp1 = file.getValue("abc");
+	if (tmp1 !== 456)
+		throw "Error: check3.tmp1";
+	var tmp2 = file.getValue("t");
+	if (tmp2 !== false)
+		throw "Error: check3.tmp2";
+	var tmp3 = file.getValue("123");
+	if (tmp3 !== null)
+		throw "Error: check3.tmp3";
+	console.log("Check 3 passed. Update and and delete is working.");
+	finish();
+}
+
+function finish() {
+	console.log("All tests pass. Code appears to be working as desired!. Deleteing test file now.");
+	deleteFile();
+}
+
+//Remove file when complete
+function deleteFile() {
+	fs.unlink(filename, function(err) {
+		if (err)
+			throw err;
+		console.log('File successfully deleted.');
+	});
+}
+
+//Random functions
+function getRandomText(numberOfCharactersLow, numberOfCharactersHigh) {
+	var numbChar = getRandomInt(numberOfCharactersLow, numberOfCharactersHigh);
+	var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+	var returnText = '';
+	for (var i = 0; i < numbChar; i++) {
+		returnText += possible.charAt(getRandomInt(0, 62));
+	}
+	return returnText;
+}
+
+function getRandomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
